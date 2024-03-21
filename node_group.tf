17afe461261b23c91041b6c3a4a43d6693f358c9
Alex Donec <alex.donec@pinely.com>
2024-03-21 17:17:03 +0300

    Change to node_name

--- node_group.tf
+++ node_group.tf
@@ -26,7 +26,7 @@ resource "nebius_kubernetes_node_group" "kube_node_groups" {
 
     platform_id = lookup(each.value, "platform_id", var.node_groups_defaults.platform_id)
 
-    name     = lookup(each.value, "name", var.node_groups_defaults.name)
+    name     = lookup(each.value, "node_name", var.node_groups_defaults.node_name)
     metadata = lookup(each.value, "metadata", var.node_groups_defaults.metadata)
     resources {
       cores         = lookup(each.value, "node_cores", var.node_groups_defaults.node_cores)
